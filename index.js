69e917a6be49e09357d37dd18c4a2fd5b736b4c3
hillarioh <okerioh@gmail.com>
2020-08-10 16:32:24 +0300

    add dom elements

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ const wMain = document.getElementById('w-main');
 const cityName = document.getElementById('city-name');
 const wTemp =document.getElementById('w-temp');
 const wDate =document.getElementById('w-date');
+const wSunrise = document.getElementById('w-sunrise');
+const wSunset = document.getElementById('w-sunset');
+const wSpeed = document.getElementById('w-speed');
+const wPressure = document.getElementById('w-pressure');
+const wDeg = document.getElementById('w-deg');
+const wHumidity = document.getElementById('w-humidity');
 
 myForm.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("enetred values");
     myContainer.classList.add('active');
     submitInput.classList.add('hidden');
-    setTimeout(() => { 
-        weatherCurrent.classList.remove('hidden');
-        submitCity(cityInput.value);
-     }, 500);
+    submitCity(cityInput.value);
+    myForm.reset();
+    // setTimeout(() => { }, 500);
 });
 
 const makeRequest = (city)=>{
@@ -37,10 +42,17 @@ async function submitCity(city) {
 
     try {
         const cityResponse = await makeRequest(city);
+        weatherCurrent.classList.remove('hidden');
         wMain.textContent = cityResponse.weather[0].main;
         cityName.textContent = cityResponse.name;
         wTemp.textContent = cityResponse.main.temp;
-        wDate.textContent = Date.now();     
+        wDate.textContent = Date.now();
+        wSunrise.textContent = cityResponse.sys.sunrise;
+        wSunset.textContent = cityResponse.sys.sunset;
+        wSpeed.textContent = cityResponse.wind.speed;
+        wPressure.textContent = cityResponse.main.pressure;
+        wDeg.textContent = cityResponse.wind.deg;
+        wHumidity.textContent = cityResponse.main.humidity;
     } catch (error) {
         wMain.textContent = error.error;                     
     }
